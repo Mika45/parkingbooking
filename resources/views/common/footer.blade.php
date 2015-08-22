817d780278ae4b6f5b52fb954c78c347a38cdf68
tmihalis <tmihalis@server24.cretaforce.gr>
2015-08-22 20:40:24 +0300

    Extra social buttons added

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -10,6 +10,16 @@
 			              		<li><a href="/{{App::getLocale()}}/privacy">{{Lang::get('site.nav_privacy')}}</a></li>
 			              		<li><a href="/{{App::getLocale()}}/payment-methods">{{Lang::get('site.nav_payment')}}</a></li>
 			              		<li class="pull-right">
+			              			<a class="btn btn-social-icon btn-twitter" target="_blank" href="https://twitter.com/ParkingLegend">
+			              				<i class="fa fa-twitter"></i>
+			              			</a>
+			              		</li>
+			              		<li class="pull-right">
+			              			<a class="btn btn-social-icon btn-google" target="_blank" href="https://plus.google.com/+Parkinglegendcom">
+			              				<i class="fa fa-google"></i>
+			              			</a>
+			              		</li>
+			              		<li class="pull-right">
 			              			<a class="btn btn-social-icon btn-facebook" target="_blank" href="https://www.facebook.com/ParkingLegend.com1">
 			              				<i class="fa fa-facebook"></i>
 			              			</a>
