24132dbf5dc6ee7cc3feedfa967740489aa6dba9
tmihalis <tmihalis@server24.cretaforce.gr>
2015-09-08 23:37:50 +0300

    Correct config for database.php

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'tmihalis_parkdem'),
-			'username'  => env('DB_USERNAME', 'tmihalis_parkdem'),
-			'password'  => env('DB_PASSWORD', 'p4rkd3m'),
+			'host'      => env('DB_HOST'),
+			'database'  => env('DB_DATABASE'),
+			'username'  => env('DB_USERNAME'),
+			'password'  => env('DB_PASSWORD'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
