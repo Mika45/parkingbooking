361587c23bccd858eca8c36c81aeaf4ee3ecde15
tmihalis <tmihalis@server24.cretaforce.gr>
2015-09-23 00:44:20 +0300

    add date in article page

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -15,6 +15,7 @@
 	{{-- @foreach ($article as $art) --}}
 
 		<h2>{{ $article->title }}</h2>
+		<h5>{{ date('d/m/Y', strtotime($article->published_at)) }}</h5>
 		<p>{!! $article->body !!}</p>
 
 	{{-- @endforeach --}}
