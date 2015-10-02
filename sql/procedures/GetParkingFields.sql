591e07552b4662a8f9470d0e347c901438f31338
tmihalis <tmihalis@server24.cretaforce.gr>
2015-10-03 01:26:42 +0300

    update GetParkingFields to order by sortorder

--- sql/procedures/GetParkingFields.sql
+++ sql/procedures/GetParkingFields.sql
@@ -5,7 +5,8 @@ BEGIN
 	SELECT pf.parking_id, pf.field_id, f.field_name, pf.required, f.type, f.attributes, f.label
 	FROM   PARKING_FIELD pf, FIELD f
 	WHERE  pf.field_id = f.field_id
-	AND    parking_id = in_parking_id;
+	AND    parking_id = in_parking_id
+	ORDER  BY -f.sortorder DESC;
 
 END$$
 DELIMITER ;
