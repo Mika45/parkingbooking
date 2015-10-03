0155761cf52256ad9a019c1521a1808ca0e29df1
tmihalis <tmihalis@server24.cretaforce.gr>
2015-10-04 02:18:07 +0300

    enable retrieval of MyBookings via email

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -64,7 +64,7 @@ class UsersController extends Controller {
 		$userId = Auth::user()->user_id;
 		$user = User::findOrFail($userId);
 
-		$bookings = DB::select('CALL GetMyBookings("'.$userId.'")');
+		$bookings = DB::select('CALL GetMyBookings("'.$user->email.'")');
 
 		$locationsList = get_locations_for_search();
 
--- sql/procedures/GetMyBookings.sql
+++ sql/procedures/GetMyBookings.sql
@@ -1,5 +1,5 @@
 DELIMITER $$
-CREATE DEFINER=`tmihalis_park`@`localhost` PROCEDURE `GetMyBookings`(IN `in_user_id` VARCHAR(50) )
+CREATE DEFINER=`tmihalis_park`@`localhost` PROCEDURE `GetMyBookings`(IN `in_email` VARCHAR(50) )
 BEGIN
 
 	SELECT b.booking_id, 
@@ -8,13 +8,12 @@ BEGIN
 		   DATE_FORMAT(b.checkout, '%d/%m/%Y %H:%i') AS checkout, 
 		   b.price, 
 		   b.car_reg,
-		   #DATE_FORMAT(b.created_at, '%d/%m/%Y %H:%i') AS booked_at,
-         DATE_FORMAT(b.created_at, '%d/%m/%Y') AS booked_at,
+		   DATE_FORMAT(b.created_at, '%d/%m/%Y') AS booked_at,
 		   p.parking_name
 	FROM   BOOKING b,
 		   PARKING p
 	WHERE  b.parking_id = p.parking_id
-	AND	   b.user_id = in_user_id
+	AND	   b.email = in_email
 	AND	   IFNULL(b.status,'A') != 'C' /* Not Cancelled */
 	ORDER  BY b.booking_id DESC;
 
