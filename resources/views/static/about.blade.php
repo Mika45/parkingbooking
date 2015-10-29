a4da6d6c77f8ffe68c051f7a9eed84dd97be528f
tmihalis <tmihalis@server24.cretaforce.gr>
2015-10-30 01:03:58 +0200

    added image in about page

--- resources/views/static/about.blade.php
+++ resources/views/static/about.blade.php
@@ -4,6 +4,11 @@
     {{Lang::get('site.page_about')}}
 @stop
 
+@section('sidebar')
+	<h1>&nbsp;</h1>
+	<img src="/img/about.png"/>
+@stop
+
 @section('content')
 
 	<h1>{{Lang::get('content.about_heading')}}</h1>
