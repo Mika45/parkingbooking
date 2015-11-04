1824b5c38ea6f0e2d202483a2f033c776332866b
tmihalis <tmihalis@server24.cretaforce.gr>
2015-11-05 01:09:52 +0200

    updated the sql file for the previous change in mybookings

--- sql/procedures/GetMyBookings.sql
+++ sql/procedures/GetMyBookings.sql
@@ -3,18 +3,20 @@ CREATE DEFINER=`tmihalis_park`@`localhost` PROCEDURE `GetMyBookings`(IN `in_emai
 BEGIN
 
 	SELECT b.booking_id, 
-		   b.parking_id, 
-		   DATE_FORMAT(b.checkin, '%d/%m/%Y %H:%i') AS checkin, 
-		   DATE_FORMAT(b.checkout, '%d/%m/%Y %H:%i') AS checkout, 
-		   b.price, 
-		   b.car_reg,
-		   DATE_FORMAT(b.created_at, '%d/%m/%Y') AS booked_at,
-		   p.parking_name
+		    b.parking_id, 
+		    DATE_FORMAT(b.checkin, '%d/%m/%Y %H:%i') AS checkin, 
+		    DATE_FORMAT(b.checkout, '%d/%m/%Y %H:%i') AS checkout, 
+		    b.price, 
+		    b.car_reg,
+		    DATE_FORMAT(b.created_at, '%d/%m/%Y') AS booked_at,
+		    p.parking_name,
+          CASE WHEN checkin < SYSDATE() THEN 'N' ELSE 'Y' END AS can_view,
+          CASE WHEN checkin < SYSDATE() THEN 'N' ELSE 'Y' END AS can_amend
 	FROM   BOOKING b,
-		   PARKING p
+		    PARKING p
 	WHERE  b.parking_id = p.parking_id
-	AND	   b.email = in_email
-	AND	   IFNULL(b.status,'A') != 'C' /* Not Cancelled */
+	AND	 b.email = in_email
+	AND	 IFNULL(b.status,'A') != 'C' /* Not Cancelled */
 	ORDER  BY b.booking_id DESC;
 
 END$$
