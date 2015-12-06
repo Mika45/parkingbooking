43109606d9526b865e7b161621b53a380ca93fe8
tmihalis <tmihalis@server24.cretaforce.gr>
2015-12-06 23:55:13 +0200

    cleaned file from unnecessary comments

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -4,18 +4,19 @@
 		<title>ParkingLegend.com: @yield('title')</title>
 		<meta name="google-site-verification" content="JRnT1QScbmi8ajN_F8ZzCAXr7gwQVbgj1-Ub5DbdVpk" />
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta charset="utf-8" />
+
 		{!! HTML::script('js/jquery-1.11.2.min.js') !!}
 
 		{!! HTML::script('js/jquery-migrate-1.2.1.min.js') !!}
 		{!! HTML::script('js/moment.js') !!}
 		{!! HTML::style('css/bootswatch.min.css') !!}
-		<!-- Latest compiled and minified JavaScript -->
 		{!! HTML::script('js/bootstrap.js') !!}
-		<!-- Latest compiled and minified CSS -->
+
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 		
 		<!-- Optional theme -->
-		<!--{!! HTML::style('css/bootstrap-theme.css') !!}-->
 		{!! HTML::style('css/bootstrap-theme2.css') !!}
 		{!! HTML::script('js/bootstrap-datetimepicker.js') !!}
 		{!! HTML::style('css/bootstrap-datetimepicker.css') !!}
@@ -39,8 +40,6 @@
 
 		<style>
 			.bs-glyphicons-list{padding-left:0;list-style:none}
-			/*.bs-glyphicons li{float:left;width:25%;height:115px;padding:10px;font-size:10px;}*/
-			/*.glyphicon-ok{margin-top:5px;margin-bottom:10px;font-size:24px}*/
 		</style>
 
 	</head>
@@ -101,11 +100,10 @@
 					          	</ul>
 					        </li>
 		            	@else
-		            		<!--<li>{{link_to('login', 'Login')}}</li>-->
 		            		<li><a href="/auth/login">{{Lang::get('site.nav_login')}}</a></li>
 		            		<li><a href="/auth/register">{{Lang::get('site.nav_register')}}</a></li>
 		            	@endif
 				    </ul>
-	          	</div><!--/.nav-collapse -->
-	        </div><!--/.container-fluid -->
+	          	</div>
+	        </div>
 	    </nav>
