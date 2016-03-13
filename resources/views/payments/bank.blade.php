71ab3e7354855a97da6f09cd5964a1088c6b375e
tmihalis <tmihalis@server24.cretaforce.gr>
2016-03-13 21:45:33 +0200

    passing of online config params to the form

--- resources/views/payments/bank.blade.php
+++ resources/views/payments/bank.blade.php
@@ -3,10 +3,10 @@
 @section('content')
 
 	<form action='https://paycenter.piraeusbank.gr/redirection/pay.aspx' method='post' name='frm' target="pay">
-		<input name="AcquirerId" type="hidden" value="14" /> 
-		<input name="MerchantId" type="hidden" value="2133613386" /> 
-		<input name="PosId" type="hidden" value="2139909353" /> 
-		<input name="User" type="hidden" value="KA009598" /> 
+		<input name="AcquirerId" type="hidden" value="{{$config['ONLINE_ACQUIRER_ID']}}" /> 
+		<input name="MerchantId" type="hidden" value="{{$config['ONLINE_ACQUIRER_ID']}}" /> 
+		<input name="PosId" type="hidden" value="{{$config['ONLINE_ACQUIRER_ID']}}" /> 
+		<input name="User" type="hidden" value="{{$config['ONLINE_ACQUIRER_ID']}}" /> 
 		<input name="LanguageCode" type="hidden" value="el-GR" /> 
 		<input name="MerchantReference" type="hidden" value="PL1506" /> 
 		<input name="ParamBackLink" type="hidden" value="p1=v1&p2=v2" /> 
