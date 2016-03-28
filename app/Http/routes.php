59cac42f1a025bde933cbcf6df4d96a67356bdf6
tmihalis <tmihalis@server24.cretaforce.gr>
2016-03-29 00:00:39 +0300

    token mismatch error test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -97,8 +97,8 @@ Route::group(['prefix' => LaravelLocalization::setLocale(), 'middleware' => ['se
 	Route::get('checkout', 'ParkingsController@checkout');
 	Route::post('checkout', 'ParkingsController@checkout');
 	//Route::get('payment/online', 'PaymentsController@bank');
-	//Route::get('payment/result/{name?}', 'PaymentsController@result');
-	Route::post('payment/result/{name?}', 'PaymentsController@result');
+	Route::get('payment/result/{name?}', 'PaymentsController@result');
+	//Route::post('payment/result/{name?}', 'PaymentsController@result');
 	
 	Route::get('settings', ['middleware' => 'auth', 'uses' => 'UsersController@index']);
 	Route::post('settings', 'UsersController@update');
